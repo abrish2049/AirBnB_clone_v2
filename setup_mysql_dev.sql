6d5340498946758b4ca0ae7663dd2a58453897bb
abrish2049 <abrahammenberu@gmail.com>
2022-10-15 10:58:06 +0000

    db

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,19 +1,9 @@
--- Creates a MySQL server with:
---   Database hbnb_dev_db.
---   User hbnb_dev with password hbnb_dev_pwd in localhost.
---   Grants all privileges for hbnb_dev on hbnb_dev_db.
---   Grants SELECT privilege for hbnb_dev on performance_schema.
+-- Set ALL privileges on the database `hbnb_dev_db` to the `hbnb_dev` user.
+-- Set SELECT privileges on the database `performance_schema` to the `hbnb_dev` user.
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+DROP USER IF EXISTS 'hbnb_dev'@'localhost';
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
